7ce7c4b6a14e4db5060713afca3c23f2f9b23c3e
caochangkui <1243960844@qq.com>
2018-06-28 14:52:34 +0800

    fix bug

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -16,7 +16,7 @@
           </div>
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
-            <p class="item-desc">{item.desc}</p>
+            <p class="item-desc">{{item.desc}}</p>
             <button class="item-button">查看详情</button>
           </div>
         </router-link>
